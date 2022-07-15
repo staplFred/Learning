50c665f6367b279e7254ab74f4c318d971fb9ac6
cj738 <cj738@msstate.edu>
2022-07-15 15:55:42 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function myFunction(){
 }
 const fortune =[
     "The fortune you seek is in another cookie",
-    "Aclosed mouth gathers no feet",
+    "A closed mouth gathers no feet",
     "A conclusion is simply the place where you got tired of thinking",
     "A cynic is only a frustrated optimist",
     "A foolish man listens to his heart",
@@ -16,4 +16,16 @@ const fortune =[
 const results = randomFortune(fortune);
 
     document.getElementById('demo').innerHTML = results;
+};
+function mySecondFunction(){ 
+const fortune =[
+    "The fortune you seek is in another cookie",
+    "A closed mouth gathers no feet",
+    "A conclusion is simply the place where you got tired of thinking",
+    "A cynic is only a frustrated optimist",
+    "A foolish man listens to his heart",
+    "Happy days are ahead",
+];
+
+    document.getElementById('demo').innerHTML = fortune[1];
 };
